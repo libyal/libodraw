33b6b40ca62b296eb8a464e9718c06acc8d06e77
Joachim Metz <joachim.metz@gmail.com>
2015-01-05 19:01:57 +0100

    Applied updates.

--- odrawtools/odrawinfo.c
+++ odrawtools/odrawinfo.c
@@ -1,7 +1,7 @@
 /*
  * Shows information obtained from optical disc (split) RAW file(s)
  *
- * Copyright (c) 2010-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
