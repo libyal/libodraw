eb216f3ce5971db60d783babb715c781d901bef4
Joachim Metz <joachim.metz@gmail.com>
2020-03-16 05:47:17 +0100

    Applied updates

--- libodraw/libodraw.c
+++ libodraw/libodraw.c
@@ -1,7 +1,7 @@
 /*
  * Library to access optical disc (split) RAW formats
  *
- * Copyright (C) 2010-2019, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2020, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- odrawtools/odrawverify.c
+++ odrawtools/odrawverify.c
@@ -1,7 +1,7 @@
 /*
  * Verifies the integrity of optical disc (split) RAW file(s)
  *
- * Copyright (C) 2010-2019, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2020, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
