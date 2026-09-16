97cc1ccf8b9fe70499f1698a5a678beea0e8f3c1
Joachim Metz <joachim.metz@gmail.com>
2026-09-16 18:52:52 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libodraw/.libs/libodraw.1.dylib && test -f ./pyodraw/.libs/pyodraw.so
+if [ -f "${PWD}/libodraw/.libs/libodraw.1.dylib" ] && [ -f ./pyodraw/.libs/pyodraw.so ]
 then
-	install_name_tool -change /usr/local/lib/libodraw.1.dylib ${PWD}/libodraw/.libs/libodraw.1.dylib ./pyodraw/.libs/pyodraw.so
+    install_name_tool -change /usr/local/lib/libodraw.1.dylib "${PWD}/libodraw/.libs/libodraw.1.dylib" ./pyodraw/.libs/pyodraw.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
